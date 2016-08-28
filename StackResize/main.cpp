e8bbe8a869cb54a3fdc376fb9e856687b56f6d04
John Zhen M <kingdomlearke@hotmail.com>
2016-08-27 20:57:30 -0700

    Merge remote-tracking branch 'refs/remotes/Ant2888/master' into experimental
    # Conflicts:
    #	NMSE_Core_1_0/AddonManager.cpp
    #	NMSE_Core_1_0/ModAPI.h
    #	NMSE_Libs/MemoryManager.cpp
    #	NMSExtender.v12.suo

--- StackResize/main.cpp
+++ StackResize/main.cpp
@@ -22,23 +22,28 @@ SizeSettings GetSettings(LPCSTR dir) {
 	return {
 		(int)GetPrivateProfileInt("SIZE_SETTINGS", "DEFAULT_SUBS_SIZE", 250, dir),
 		(int)GetPrivateProfileInt("SIZE_SETTINGS", "DEFAULT_ITEM_SIZE", 1, dir),
-		(int)GetPrivateProfileInt("SIZE_SETTINGS", "SHIP_MULT", 2, dir),
-		(int)GetPrivateProfileInt("SIZE_SETTINGS", "CHARGE_VAL", -1, dir)
+		(int)GetPrivateProfileInt("SIZE_SETTINGS", "SHIP_MULT",			2, dir),
+		(int)GetPrivateProfileInt("SIZE_SETTINGS", "CHARGE_VAL",		-1, dir)
 	};
 }
 
 extern "C"
 {
-	bool OnStart(HMODULE& mHandle, ModDetails& info, Memory& mem){
+	bool OnStart(HMODULE& mHandle, ModDetails& info){
 		localH = mHandle;
 		info.name = "Gula";
 		vers = info.version; 
-		local_Memory = mem.local;
-		global_Memory = mem.global;
 
-		settings = GetSettings("./NMSE/Gula.ini");
+		settings = GetSettings("./NMSE/StackResize.ini");
 
 		hMonitor = CreateThread(0, 0, ThreadProc, 0, 0, 0);
 		return true;
+	}
+
+	bool GrabVirtualMem(MemoryManager* globalMem, MemoryManager* localMem) {
+		global_Memory = *globalMem;
+		local_Memory = *localMem;
+
+		return globalMem != NULL && localMem != NULL;
 	}
 };
